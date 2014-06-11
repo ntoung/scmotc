b8e14dbd8cb2b87b3bb9274652ce733c37bc4712
Nathan Toung <nmtoung@gmail.com>
2014-06-10 21:12:22 -0700

    css, some progress on form

--- userSchema.js
+++ userSchema.js
@@ -1,9 +1,13 @@
 console.log("Initializing userSchema.js")
 
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 var userSchema = mongoose.Schema({
 	name: String
+	//password: String
+	// username: { type: String, required: true, index: { unique: true } },
+    // password: { type: String, required: true }
 })
 
 userSchema.methods.speak = function speak() {
@@ -13,22 +17,15 @@ userSchema.methods.speak = function speak() {
 	console.log(greeting);
 }
 
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 10;
- 
-var UserSchema = new Schema({
-    username: { type: String, required: true, index: { unique: true } },
-    password: { type: String, required: true }
-});
- 
-UserSchema.pre(save, function(next) {
-    var user = this;
- 
-	// only hash the password if it has been modified (or is new)
+// SALT Password Hash
+userSchema.pre('save', function(next) {
+	console.log('Entering userSchema.save()');	
+	var user = this;
+	var SALT_WORK_FACTOR = 10;
+
+	// only hash password if new or modified
 	if (!user.isModified('password')) return next();
-	 
+
 	// generate a salt
 	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
 	    if (err) return next(err);
@@ -42,13 +39,13 @@ UserSchema.pre(save, function(next) {
 	        next();
 	    });
 	});
-});
+})
  
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
         if (err) return cb(err);
         cb(null, isMatch);
     });
-};
+}
  
-module.exports = mongoose.model(User, UserSchema);
+exports.User = mongoose.model('User', userSchema);
